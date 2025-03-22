9121442febdde760e285abf9ffd33d3c4ccda266
Afsal T A <afsal@finanshels.com>
2025-03-23 03:54:30 +0530

    Update App.py

--- App.py
+++ App.py
@@ -56,7 +56,7 @@ st.markdown("""
             text-shadow: 0 0 05px ##886dc7, 0 0 10px ##cdb4d4;
         }
         to {
-            text-shadow: 0 0 10px #6C0345, 0 0 2px #FF7EE2;
+            text-shadow: 0 0 50px #6C0345, 0 0 2px #FF7EE2;
         }
     }
     </style>
