e41e6cf0ca50f67782c4031212a4fb24c4e0f931
Afsal T A <afsal@finanshels.com>
2025-03-24 00:51:01 +0530

    Update App.py

--- App.py
+++ App.py
@@ -11,7 +11,7 @@ import adib_bank
 # ==== Bank Mapping ====
 bank_modules = {
     "🟢 RAK Bank": Rak_Bank,
-    "🟢 Al Jazira Bank - Coming soon": al_jazira_bank,
+    "🟢 Al Jazira Bank - "Coming soon"": al_jazira_bank,
     "🟢 Emirates Islamic Bank": emirates_islamic_bank,
     "🟢 FAB Bank": fab_bank,
     "🟢 WIO Bank": Wio_bank,
